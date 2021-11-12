05a0646038afe66a425f8a383ce48d645b913e39
nadakuditiganeshkumar <nadakuditi.ganeshkumar@gmail.com>
2021-11-12 13:55:54 +0530

    first commit

---
+++ store/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from store.models import Product,Variation
+
+
+# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=('product_name','price','stock','is_available','category','modified_date')
+    prepopulated_fields ={'slug':('product_name',)}
+admin.site.register(Product,ProductAdmin)
+
+#Variation
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active')
+    list_editable = ('is_active',)
+    list_filter = ('product', 'variation_category', 'variation_value', 'is_active')
+admin.site.register(Variation,VariationAdmin)
